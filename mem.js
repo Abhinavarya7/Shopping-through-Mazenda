61a0ca1d38efc29a8d5448b04d856baa0cd26162
Abhinav <abhinavarya731@gmail.com>
2022-09-14 08:32:28 +0530

    Initial commit

---
+++ mem.js
@@ -0,0 +1,11 @@
+function chbg() {
+    b=document.getElementById('b');
+    b.style.backgroundColor = 'rgb(182, 212, 230)';
+    document.getElementById('c').style.backgroundColor = 'rgb(182, 212, 230)';
+    console.log("mouseenter");
+ } 
+function bt(){
+    document.getElementById('b').style.backgroundColor = 'white';
+    document.getElementById('c').style.backgroundColor = 'white';
+}
+ console.log("linked")
